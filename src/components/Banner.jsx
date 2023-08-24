f994d8135a596f43ae5a35ee0e369ab509041818
Vitor Santos <vitorsantos.seo@gmail.com>
2023-08-24 16:26:49 -0300

    fix visual issues with grid on dashboard , home page and filter page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ import styles from './Banner.css'
 const Banner = ({ titulo, descricao}) => {
     return(
         <div className="banner">
-            <img src="https://images.unsplash.com/photo-1610165539750-051008c3e799?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" />
+            <img src="https://www.vitoriaparkshopping.com/wp-content/uploads/2020/01/logo-airsoft.png" alt="" />
             <div>
             <h2>{titulo} </h2>
             <p> {descricao}</p>
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -5,6 +5,8 @@ import { NavLink } from 'react-router-dom';
 import { useAuthValue } from '../../context/AuthContext'
 import { useFechdocuments } from '../../hooks/useFechdocuments'
 
+import Banner from '../../components/Banner'
+
 const Dashboard = () => {
 
   const { user } = useAuthValue() 
@@ -15,6 +17,9 @@ const Dashboard = () => {
 
   return (
     <div className='dashboard-container'>
+
+      <Banner titulo={'Dashboard'} descricao={'Pronto para gerenciar seus eventos?'}/>
+
       <h2>{user && user.displayName },<br/> conheça seu dashboard</h2>
       <p>Aqui você terá controle de todas as suas atividades, eventos e relatórios</p>
 
